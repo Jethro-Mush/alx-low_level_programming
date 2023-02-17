fce1d0e207e353170531571df980001a600be373
Jethro-Mush <mushilingwaj@gmail.com>
2023-02-17 12:00:24 -0800

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 /*
  * File: 9-print_comb.c
- * Author: Jethro
+ * Auth:  Jethro Mushilingwa
  */
 
 #include <stdio.h>
@@ -10,19 +10,22 @@
  *
  * Return: Always 0.
  */
-int main(void) {
-  int num;
+int main(void)
+{
+	int num;
 
-  for (num = 0; num <= 9; num++)
-  {
-    putchar((num % 10) + '0');
-    if (num == 9)
-      continue;
-    putchar(',');
-    putchar(' ');
-  }
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
-  putchar('\n');
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
+
