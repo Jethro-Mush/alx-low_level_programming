70f2014e18aca14bac716638b2fbfe9894ce457f
Jethro-Mush <mushilingwaj@gmail.com>
2023-02-21 06:10:16 -0800

    changes

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-holberton.c
+ * Author: Jethro
+ *
+ */
+
+#include "holberton.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
