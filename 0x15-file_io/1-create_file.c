9e98d4f6be81a78cc620c7af9513c686f75ce386
Jethro-Mush <mushilingwaj@gmail.com>
2023-04-13 08:10:14 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * create_file - Creates a file and writes the provided text content to it.
  * @filename: The name of the file to create.
@@ -10,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
     int file_descriptor;
-    int num_letters;
+    int letters;
     int write_result;
 
     if (!filename)
@@ -19,21 +18,20 @@ int create_file(const char *filename, char *text_content)
     file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
     if (file_descriptor == -1)
-        return (-1);
+    		return (-1);
 
     if (!text_content)
-        text_content = "";
+    		text_content = "";
 
-    for (num_letters = 0; text_content[num_letters]; num_letters++)
+    for (letters = 0; text_content[letters]; letters++)
         ;
 
-    write_result = write(file_descriptor, text_content, num_letters);
+    write_result = write(file_descriptor, text_content, letters);
 
     if (write_result == -1)
-        return (-1);
+    		return (-1);
 
     close(file_descriptor);
 
     return (1);
 }
-
