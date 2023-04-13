bfca8802ef8629c4992c9363ac61e6918b475006
Jethro-Mush <mushilingwaj@gmail.com>
2023-04-13 08:13:58 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,30 +8,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor;
-    int letters;
-    int write_result;
+	int fd;
+	int nletters;
+	int rwr;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (file_descriptor == -1)
-    		return (-1);
+	if (fd == -1)
+		return (-1);
 
-    if (!text_content)
-    		text_content = "";
+	if (!text_content)
+		text_content = "";
 
-    for (letters = 0; text_content[letters]; letters++)
-        ;
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-    write_result = write(file_descriptor, text_content, letters);
+	rwr = write(fd, text_content, nletters);
 
-    if (write_result == -1)
-    		return (-1);
+	if (rwr == -1)
+		return (-1);
 
-    close(file_descriptor);
+	close(fd);
 
-    return (1);
+	return (1);
 }
