eb4acc48eea2618b2c09193aceb73f474732d10d
Jethro-Mush <mushilingwaj@gmail.com>
2023-04-13 08:46:12 -0700

    changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,13 +8,13 @@
 
 /**
  * print_addr - prints address
- * @ptr: pointer to ELF header
- *
- * Return: no return
+ * @ptr: magic.
+ * Return: no return.
  */
 void print_addr(char *ptr)
 {
-	int i, begin;
+	int i;
+	int begin;
 	char sys;
 
 	printf("  Entry point address:               0x");
@@ -42,8 +42,10 @@ void print_addr(char *ptr)
 		{
 			if (ptr[i] >= 0)
 				printf("%02x", ptr[i]);
+
 			else if (ptr[i] < 0)
 				printf("%02x", 256 + ptr[i]);
+
 		}
 	}
 	printf("\n");
@@ -51,9 +53,8 @@ void print_addr(char *ptr)
 
 /**
  * print_type - prints type
- * @ptr: pointer to ELF header
- *
- * Return: no return
+ * @ptr: magic.
+ * Return: no return.
  */
 void print_type(char *ptr)
 {
@@ -81,9 +82,8 @@ void print_type(char *ptr)
 
 /**
  * print_osabi - prints osabi
- * @ptr: pointer to ELF header
- *
- * Return: no return
+ * @ptr: magic.
+ * Return: no return.
  */
 void print_osabi(char *ptr)
 {
@@ -105,9 +105,8 @@ void print_osabi(char *ptr)
 
 /**
  * print_version - prints version
- * @ptr: pointer to ELF header
- *
- * Return: no return
+ * @ptr: magic.
+ * Return: no return.
  */
 void print_version(char *ptr)
 {
@@ -120,12 +119,10 @@ void print_version(char *ptr)
 
 	printf("\n");
 }
-
 /**
  * print_data - prints data
- * @ptr: pointer to ELF header
- *
- * Return: no return
+ * @ptr: magic.
+ * Return: no return.
  */
 void print_data(char *ptr)
 {
@@ -138,8 +135,112 @@ void print_data(char *ptr)
 	if (data == 2)
 		printf(", big endian\n");
 }
+/**
+ * print_magic - prints magic info.
+ * @ptr: magic.
+ * Return: no return.
+ */
+void print_magic(char *ptr)
+{
+	int bytes;
+
+	printf("  Magic:  ");
+
+	for (bytes = 0; bytes < 16; bytes++)
+		printf(" %02x", ptr[bytes]);
+
+	printf("\n");
+
+}
 
 /**
- * print_magic - prints magic info
- * @ptr: pointer to
+ * check_sys - check the version system.
+ * @ptr: magic.
+ * Return: no return.
+ */
+void check_sys(char *ptr)
+{
+	char sys = ptr[4] + '0';
 
+	if (sys == '0')
+		exit(98);
+
+	printf("ELF Header:\n");
+	print_magic(ptr);
+
+	if (sys == '1')
+		printf("  Class:                             ELF32\n");
+
+	if (sys == '2')
+		printf("  Class:                             ELF64\n");
+
+	print_data(ptr);
+	print_version(ptr);
+	print_osabi(ptr);
+	print_type(ptr);
+	print_addr(ptr);
+}
+
+/**
+ * check_elf - check if it is an elf file.
+ * @ptr: magic.
+ * Return: 1 if it is an elf file. 0 if not.
+ */
+int check_elf(char *ptr)
+{
+	int addr = (int)ptr[0];
+	char E = ptr[1];
+	char L = ptr[2];
+	char F = ptr[3];
+
+	if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd, ret_read;
+	char ptr[27];
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Err: file can not be open\n");
+		exit(98);
+	}
+
+	lseek(fd, 0, SEEK_SET);
+	ret_read = read(fd, ptr, 27);
+
+	if (ret_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
+		exit(98);
+	}
+
+	if (!check_elf(ptr))
+	{
+		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
+		exit(98);
+	}
+
+	check_sys(ptr);
+	close(fd);
+
+	return (0);
+}
